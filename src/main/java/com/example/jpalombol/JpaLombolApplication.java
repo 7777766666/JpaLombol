5d8b2d262183ad90ed58987bcc2e43863d424e0e
777 <goszakupki.aktiv@gmail.com>
2023-05-29 15:45:49 +0300

    lol

---
+++ src/main/java/com/example/jpalombol/JpaLombolApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpalombol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLombolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLombolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpalombol/JpaLombolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpalombol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLombolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
